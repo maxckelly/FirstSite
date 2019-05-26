c50138f41a6d5eec8a9365edadd1609b1c226928
Max Kelly <max@revelthat.com>
2019-05-27 09:39:24 +1000

    replaced server.js file with teachers code.

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const app = express();
-//Setup environment varibles 
 
-app.engine('handlebars', exphbs ({
+const app = express();
+// setup template engine
+app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
-
 app.set('view engine', 'handlebars');
 // setup express static public folder for css and js and images
-
-app.use(express.static ('public')); 
-//This links to the public folder then links to the css style sheet.
-
+app.use(express.static('public'));
+// setup environment virables
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res ) => {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
@@ -23,10 +20,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/contact', (req,res) => {
-    res.render('contact')
+app.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`);
 });
